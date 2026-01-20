1ab7a6a477903f2fafd19853baa22d5dbcce561a
amendokat <amendoimartes@gmail.com>
2026-01-19 22:02:38 -0300

    Major fixes and changes.

--- js/script.js
+++
@@ -1,6 +0,0 @@
-function copyWebsiteButton() {
-    var text =  document.getElementById("button-link");
-    text.select();
-    navigator.clipboard.writeText(text.value);
-    alert("copied! x3");
-}
